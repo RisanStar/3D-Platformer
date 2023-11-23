d2c0ec1dd24aa8cab7af54fa01056022d93a6357
RisanStar <nBuckley2023@elam.co.uk>
2023-11-23 15:39:42 +0000

    Camera, Sprint
    Sprint Mechanic + Camera movement

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float maxSpeed;
+    public float normalSpeed = 10.0f;
+    public float sprintSpeed = 20.0f;
+    public float maxSprint = 5.0f;
+    float sprintTimer;
+
+    float rotation = 0.0f;
+    float camRotation = 0.0f;
+    float camRotationSpeed = 1.5f;
+    float rotationSpeed = 2.0f;
+
+    GameObject cam;
+    Rigidbody myRigidBody;
+     void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        cam = GameObject.Find("Main camera");
+        myRigidBody = GetComponent<Rigidbody>();
+        sprintTimer = maxSprint;
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) && sprintTimer > 0.0f)
+        {
+            maxSpeed = sprintSpeed;
+            sprintTimer = sprintTimer - Time.deltaTime;
+        }else
+        {
+            maxSpeed = normalSpeed;
+            if (Input.GetKey(KeyCode.LeftShift) == false)
+            {
+                sprintTimer = sprintTimer + Time.deltaTime;
+            }
+            sprintTimer = Mathf.Clamp(sprintTimer, 0.0f, maxSprint);
+        }
+        Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed + (transform.right * Input.GetAxis("Horizontal") * maxSpeed); 
+        myRigidBody.velocity = new Vector3(newVelocity.x, myRigidBody.velocity.y, newVelocity.z);
+
+        rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
+        transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
+
+        camRotation = camRotation + Input.GetAxis("Mouse Y") * camRotationSpeed;
+        cam.transform.localRotation = Quaternion.Euler(new Vector3(camRotation, 0.0f, 0.0f));
+        camRotation = Mathf.Clamp(camRotation, -50.0f, 50.0f);
+
+
+ 
+    }
+}
